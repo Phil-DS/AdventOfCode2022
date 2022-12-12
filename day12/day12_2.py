6b45bac7dc91815a3ebe553d8e346fd48b57f22f
Phil DS <phillipdesousa9@gmail.com>
2022-12-12 18:22:09 +0000

    Speed up change: Just put all the 'a' and 'S' as 0 in the initial queue

--- day12/day12_2.py
+++ day12/day12_2.py
@@ -5,62 +5,49 @@ from queue import PriorityQueue
 maxx = len(data[0])
 maxy = len(data)
 
-inits = []
+
+testQueue = PriorityQueue()
 
 for i in range(maxy):
     for j in range(maxx):
         if data[i][j] == 'a' or data[i][j] == 'S':
-            inits.append((j,i))
-inits.sort(key=lambda x: math.sqrt(x[0]*x[0]+x[1]*x[1]))
+            testQueue.put_nowait((0,(j,i)))
 
+tested = set()
 
-best_route = math.inf
+while not testQueue.empty():
+    weight, coords = testQueue.get_nowait()
+    if coords in tested:
+        continue
 
-for init in inits:
-    testQueue = PriorityQueue()
-    testQueue.put((0,init))
-    tested = set()
+    tested.add(coords)
+    # test surrounding
+    x,y = coords
+    currentElevation = data[y][x]
 
-    while not testQueue.empty():
-        weight, coords = testQueue.get_nowait()
-        if weight > best_route:
-            # break early, its not the best route
-            break
+    if currentElevation == 'E':
+        print(weight)
+        break
 
-        if coords in tested:
+    if currentElevation == 'S':
+        currentElevation = 'a'
+    
+    l = (x-1,y)
+    r = (x+1,y)
+    u = (x,y-1)
+    d = (x,y+1)
+
+    for n in [l,r,u,d]:
+        if n in tested:
+            continue
+        nx,ny = n
+        if nx < 0 or ny < 0 or nx >= maxx or ny >= maxy:
             continue
 
-        tested.add(coords)
-        # test surrounding
-        x,y = coords
-        currentElevation = data[y][x]
+        newElevation = data[ny][nx]
+        if newElevation == 'E':
+            newElevation = 'z'
 
-        if currentElevation == 'E':
-            if weight < best_route:
-                best_route = weight
-            break
+        if ord(newElevation) - ord(currentElevation) < 2:
+            testQueue.put_nowait((weight+1,n))
 
-        if currentElevation == 'S':
-            currentElevation = 'a'
-        
-        l = (x-1,y)
-        r = (x+1,y)
-        u = (x,y-1)
-        d = (x,y+1)
-
-        for n in [l,r,u,d]:
-            if n in tested:
-                continue
-            nx,ny = n
-            if nx < 0 or ny < 0 or nx >= maxx or ny >= maxy:
-                continue
-
-            newElevation = data[ny][nx]
-            if newElevation == 'E':
-                newElevation = 'z'
-
-            if ord(newElevation) - ord(currentElevation) < 2:
-                testQueue.put_nowait((weight+1,n))
-
-        
-print(best_route)
