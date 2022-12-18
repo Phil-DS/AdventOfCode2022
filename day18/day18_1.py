a47a5cb1257c9b3069c8525aa492e1d0f3e0c030
Phil DS <phillipdesousa9@gmail.com>
2022-12-18 20:55:35 +0000

    tidy up

--- day18/day18_1.py
+++ day18/day18_1.py
@@ -3,18 +3,18 @@ from data import data
 SA = 0
 
 mask = [
-    (1,0,0),
-    (-1,0,0),
-    (0,1,0),
-    (0,-1,0),
-    (0,0,1),
-    (0,0,-1)
+    (1, 0, 0),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1)
 ]
 
-for x,y,z in data:
+for x, y, z in data:
     SA += 6
-    for dx,dy,dz in mask:
-        if (x+dx,y+dy,z+dz) in data:
+    for dx, dy, dz in mask:
+        if (x+dx, y+dy, z+dz) in data:
             SA -= 1
 
 print(SA)
